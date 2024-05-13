35d32987c4dccee69279c655a91469cd40b63f7c
Tuojing huang <a24364042@gmail.com>
2024-05-14 04:23:33 +0800

    add write csv

--- product.py
+++ product.py
@@ -7,4 +7,7 @@ while True:
     products.append([name,price])
 print(products)
 for p in products:
-    print(p[0],'的價格為',p[1])         
+    print(p[0],'的價格為',p[1])
+with open ('products.csv','w') as f:
+    for p in products:
+        f.write(p[0]+','+p[1]+'\n')            
