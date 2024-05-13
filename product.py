282fb66887ad5675d4f39e72119b26d669e2ab6b
Tuojing huang <a24364042@gmail.com>
2024-05-14 04:39:05 +0800

    add header

--- product.py
+++ product.py
@@ -8,7 +8,7 @@ while True:
 print(products)
 for p in products:
     print(p[0],'的價格為',p[1])
-with open ('products.csv','w') as f:
+with open ('products.csv','w',encoding = 'utf-8') as f:
     f.write('商品,價格\n')
     for p in products:
         f.write(p[0]+','+p[1]+'\n')            
